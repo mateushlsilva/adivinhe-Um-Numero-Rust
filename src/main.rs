602277b1b7681f63bea74b75b100707c78fa8651
mateushlsilva <mateushls01@gmail.com>
2023-02-28 16:39:55 -0300

    Adiciona rand para gerar os números aleatorios

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Adivinhe o número!");
 
     println!("Por favor, insira o seu palpite: ");
 
+    let numeroSecreto = rand::thread_rng().gen_range(1..=100);
+
     let mut numero = String::new();
 
     io::stdin()
     .read_line(&mut numero)
     .expect("Erro ao ler a linha.");
+
+    println!("O número secreto é {numeroSecreto}");
 
     println!("Você adivinhou {numero}")
 }
