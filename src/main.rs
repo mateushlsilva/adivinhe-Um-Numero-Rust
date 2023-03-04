84bb16f861dbfed55ba5fb5eeed54ed25d9c90db
mateushlsilva <mateushls01@gmail.com>
2023-03-04 17:30:48 -0300

    Finalizando o jogo

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,32 @@ use std::cmp::Ordering;
 fn main() {
     println!("Adivinhe o número!");
 
-    println!("Por favor, insira o seu palpite: ");
-
     let numero_secreto = rand::thread_rng().gen_range(1..=100);
 
-    let mut numero = String::new();
+    loop {
 
-    io::stdin()
-    .read_line(&mut numero)
-    .expect("Erro ao ler a linha.");
-
-    let numero: u32 = numero.trim().parse().expect("Por favor digite um número!");
-
-    //println!("O número secreto é {numeroSecreto}");
-    match numero.cmp(&numero_secreto) {
-        Ordering::Less => println!("Muito baixo!"),
-        Ordering::Greater => println!("Muito alto!"),
-        Ordering::Equal => println!("Você ganhou!!!"),
+        println!("Por favor, insira o seu palpite: ");
         
+        let mut numero = String::new();
+
+        io::stdin()
+        .read_line(&mut numero)
+        .expect("Erro ao ler a linha.");
+
+        let numero: u32 = match  numero.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        //println!("O número secreto é {numeroSecreto}");
+        match numero.cmp(&numero_secreto) {
+            Ordering::Less => println!("Muito baixo!"),
+            Ordering::Greater => println!("Muito alto!"),
+            Ordering::Equal => {
+                println!("Você ganhou!!!");
+                break;
+            }
+            
+        }
     }
-    println!("Você adivinhou {numero}")
 }
