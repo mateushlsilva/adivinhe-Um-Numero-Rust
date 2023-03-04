5b5b8ad7db94c90760764789077abd9168d0b036
mateushlsilva <mateushls01@gmail.com>
2023-03-04 17:12:18 -0300

    atualizando main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Adivinhe o número!");
 
     println!("Por favor, insira o seu palpite: ");
 
-    let numeroSecreto = rand::thread_rng().gen_range(1..=100);
+    let numero_secreto = rand::thread_rng().gen_range(1..=100);
 
     let mut numero = String::new();
 
@@ -14,7 +15,14 @@ fn main() {
     .read_line(&mut numero)
     .expect("Erro ao ler a linha.");
 
-    println!("O número secreto é {numeroSecreto}");
+    let numero: u32 = numero.trim().parse().expect("Por favor digite um número!");
 
+    //println!("O número secreto é {numeroSecreto}");
+    match numero.cmp(&numero_secreto) {
+        Ordering::Less => println!("Muito baixo!"),
+        Ordering::Greater => println!("Muito alto!"),
+        Ordering::Equal => println!("Você ganhou!!!"),
+        
+    }
     println!("Você adivinhou {numero}")
 }
